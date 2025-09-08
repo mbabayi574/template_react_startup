2de9797b6de1f1c087d693a19dee54cc55ff265f
Bobby <M.Babati574@Gmail.Com>
2025-09-08 11:02:13 +0330

    Add TypeScript support and update dependencies; migrate Vite config to TypeScript

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
-import { Tooltip } from 'bootstrap';
+import React, { useEffect } from "react";
+import { Tooltip } from "bootstrap";
 
 function Card(props) {
 	return (
-		<div className={'card '+ (props.className ? props.className : '')} style={props.style}>
-			{ props.children }
-			
+		<div
+			className={"card " + (props.className ? props.className : "")}
+			style={props.style}
+		>
+			{props.children}
+
 			<div className="card-arrow">
 				<div className="card-arrow-top-left"></div>
 				<div className="card-arrow-top-right"></div>
@@ -14,69 +17,86 @@ function Card(props) {
 			</div>
 		</div>
 	);
-};
+}
 
 function CardHeader(props) {
 	return (
-		<div className={'card-header '+ ((props.className) ? props.className : '') } style={props.style}>
-			{ props.children }
+		<div
+			className={"card-header " + (props.className ? props.className : "")}
+			style={props.style}
+		>
+			{props.children}
 		</div>
 	);
 }
 
 function CardBody(props) {
 	return (
-		<div className={"card-body "+ ((props.className) ? props.className : '')} style={props.style}>
-			{ props.children }
+		<div
+			className={"card-body " + (props.className ? props.className : "")}
+			style={props.style}
+		>
+			{props.children}
 		</div>
 	);
-};
+}
 
 function CardImgOverlay(props) {
 	return (
-		<div className={"card-img-overlay "+ ((props.className) ? props.className : '')} style={props.style}>
-			{ props.children }
+		<div
+			className={"card-img-overlay " + (props.className ? props.className : "")}
+			style={props.style}
+		>
+			{props.children}
 		</div>
 	);
-};
+}
 
 function CardFooter(props) {
 	return (
-		<div className={"card-footer "+ ((props.className) ? props.className : '')} style={props.style}>
-			{ props.children }
+		<div
+			className={"card-footer " + (props.className ? props.className : "")}
+			style={props.style}
+		>
+			{props.children}
 		</div>
 	);
-};
+}
 
 function CardGroup(props) {
 	return (
-		<div className={"card-group "+ ((props.className) ? props.className : '')} style={props.style}>
-			{ props.children }
+		<div
+			className={"card-group " + (props.className ? props.className : "")}
+			style={props.style}
+		>
+			{props.children}
 		</div>
 	);
-};
-
+}
 
 function CardExpandToggler(props) {
 	const tooltipRef = React.createRef();
-	
+
 	useEffect(() => {
 		new Tooltip(tooltipRef.current, {
-			title: 'Expand / Compress',
-			placement: 'bottom',
-			trigger: 'hover',
-			container: 'body'
+			title: "Expand / Compress",
+			placement: "bottom",
+			trigger: "hover",
+			container: "body",
 		});
 	});
-	
+
 	function toggleCardExpand(e) {
 		e.preventDefault();
-		
-		var target = (e.target).closest('.card');
-		var targetClass = 'card-expand';
 
-		if (document.body.classList.contains(targetClass) && target.classList.contains(targetClass)) {
-			target.removeAttribute('style');
+		var target = e.target.closest(".card");
+		var targetClass = "card-expand";
+
+		if (
+			document.body.classList.contains(targetClass) &&
+			target.classList.contains(targetClass)
+		) {
+			target.removeAttribute("style");
 			target.classList.remove(targetClass);
 			document.body.classList.remove(targetClass);
 		} else {
@@ -84,12 +104,27 @@ function CardExpandToggler(props) {
 			target.classList.add(targetClass);
 		}
 
-		window.dispatchEvent(new Event('resize'));
+		window.dispatchEvent(new Event("resize"));
 	}
-	
+
 	return (
-		<a href="#/" onClick={toggleCardExpand} ref={tooltipRef} className="text-inverse text-opacity-50 text-decoration-none"><i className="bi bi-fullscreen"></i></a>
+		<a
+			href="#/"
+			onClick={toggleCardExpand}
+			ref={tooltipRef}
+			className="text-inverse text-opacity-50 text-decoration-none"
+		>
+			<i className="bi bi-fullscreen"></i>
+		</a>
 	);
 }
 
-export { Card, CardHeader, CardBody, CardImgOverlay, CardFooter, CardGroup, CardExpandToggler };
+export {
+	Card,
+	CardHeader,
+	CardBody,
+	CardImgOverlay,
+	CardFooter,
+	CardGroup,
+	CardExpandToggler,
+};
